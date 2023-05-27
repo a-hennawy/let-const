010717239c68c5c9cfd19c3052facd53a9161145
Ahmed <a.hennawy97@hotmail.com>
2023-05-27 09:56:36 +0300

    first commit

---
+++ let-const.js
@@ -0,0 +1,12 @@
+const PI = 3.14;
+
+// 1 - var can be reassigned, redeclared, easily accessed when hoisted, while let cannot be redeclared,
+//  and will only be accessable in the the scope of the code block.
+
+// 2- var can be reassigned, redeclared, easily accessed when hoisted, while const cannot be redeclared nor reassigned,
+// and will only be accessable in the the scope of the code block.
+
+// 3 - let can be reassigned, while const cannot be redeclared nor reassigned.
+
+// 4 - It’s the way that we explain how the JS compiler works. Variables are hoisted to the top
+//  of the scope they are declared in.
